56498be22b271d67c0903d5c977a783cd4c27038
Dragon Flip <cvadymt@gmail.com>
2021-03-28 19:14:44 +0300

    Fix

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -15,6 +15,11 @@ public class Bullet : MonoBehaviour
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
+    private void Start()
+    {
+        Destroy(gameObject, 1.4F);
+    }
+
     private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
